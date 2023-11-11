55c9d2796547d494964675c07804120a7f00224a
felipe almeida batista <felipeab10@hotmail.com>
2023-11-11 09:39:56 -0400

    Configurado projeto, criado parcialmente pagina home

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,28 @@
+import { Heading, Text } from '@ignite-ui/react'
+import Image from 'next/image'
+import previewImage from '../../assets/calendar.png'
+import { Container, Hero, Preview } from './styles'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading size="4xl">Agendamento descomplicado</Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+      </Hero>
+
+      <Preview>
+        <Image
+          src={previewImage}
+          alt="Calendário simbolizando a aplicação em funcionamento"
+          height={400}
+          quality={100}
+          priority
+        />
+      </Preview>
+    </Container>
+  )
+}
